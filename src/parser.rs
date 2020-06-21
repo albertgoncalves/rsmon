8c82eb3ec8149d9d5fb36d405c40b8451c90385e
Albert Goncalves <goncalves.albert@gmail.com>
2020-06-21 12:14:25 -0400

    parse return statements

--- src/parser.rs
+++ src/parser.rs
@@ -8,6 +8,7 @@ enum Statement<'a> {
         ident: &'a str,
         value: Expression<'a>,
     },
+    Return(Expression<'a>),
 }
 
 #[derive(Debug, PartialEq)]
@@ -26,25 +27,32 @@ fn get_ident<'a, 'b>(
     }
 }
 
-macro_rules! if_semicolon {
-    ($tokens:expr, $x:expr) => {{
-        if let Some(Token::Semicolon) = $tokens.peek() {
-            return Some($x);
-        }
-    }};
-}
-
-fn get_expression<'a, 'b>(
+fn get_expr<'a, 'b>(
     tokens: &mut Peekable<Iter<'a, Token<'b>>>,
 ) -> Option<Expression<'b>> {
+    macro_rules! if_semicolon {
+        ($x:expr) => {{
+            if let Some(Token::Semicolon) = tokens.peek() {
+                return Some($x);
+            }
+        }};
+    }
     if let Some(t) = tokens.next() {
         match t {
-            Token::Num(n) => if_semicolon!(tokens, Expression::Num(*n)),
-            Token::Ident(x) => if_semicolon!(tokens, Expression::Ident(x)),
+            Token::Num(n) => if_semicolon!(Expression::Num(*n)),
+            Token::Ident(x) => if_semicolon!(Expression::Ident(x)),
             _ => (),
         }
     }
     None
+}
+
+macro_rules! break_if_not {
+    ($tokens:expr, $x:expr) => {
+        if $tokens.next() != Some(&$x) {
+            break;
+        }
+    };
 }
 
 macro_rules! push_let {
@@ -54,19 +62,27 @@ macro_rules! push_let {
         } else {
             break;
         };
-        if $tokens.next() != Some(&Token::Assign) {
+        break_if_not!($tokens, Token::Assign);
+        let value: Expression<'_> = if let Some(x) = get_expr(&mut $tokens) {
+            x
+        } else {
             break;
-        }
-        let value: Expression<'_> =
-            if let Some(x) = get_expression(&mut $tokens) {
+        };
+        break_if_not!($tokens, Token::Semicolon);
+        $ast.push(Statement::Let { ident, value });
+    }};
+}
+
+macro_rules! push_return {
+    ($tokens:expr, $ast:expr $(,)?) => {{
+        let expression: Expression<'_> =
+            if let Some(x) = get_expr(&mut $tokens) {
                 x
             } else {
                 break;
             };
-        if $tokens.next() != Some(&Token::Semicolon) {
-            break;
-        }
-        $ast.push(Statement::Let { ident, value });
+        break_if_not!($tokens, Token::Semicolon);
+        $ast.push(Statement::Return(expression));
     }};
 }
 
@@ -78,6 +94,7 @@ fn get_ast<'a>(tokens: &[Token<'a>]) -> Option<Vec<Statement<'a>>> {
             match t {
                 Token::EOF => return Some(ast),
                 Token::Let => push_let!(tokens, ast),
+                Token::Return => push_return!(tokens, ast),
                 _ => (),
             }
         } else {
@@ -105,6 +122,28 @@ mod tests {
                     ident: "y",
                     value: Expression::Ident("x"),
                 },
+            ]),
+        );
+    }
+
+    #[test]
+    fn return_statement() {
+        assert_eq!(
+            get_ast(&get_tokens("return 5;\n")),
+            Some(vec![Statement::Return(Expression::Num(5))]),
+        );
+    }
+
+    #[test]
+    fn let_return_statement() {
+        assert_eq!(
+            get_ast(&get_tokens("let x = 5;\nreturn x;\n")),
+            Some(vec![
+                Statement::Let {
+                    ident: "x",
+                    value: Expression::Num(5),
+                },
+                Statement::Return(Expression::Ident("x")),
             ]),
         );
     }
